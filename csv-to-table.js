d3f98eaa810f22ef5c35061982e3b9e73839f3b5
rkpsj <rkpsj789@gmail.com>
2017-11-27 08:21:14 +0000

    重複するコードを関数にまとめる、機能ごとに関数を分ける

--- csv-to-table.js
+++ csv-to-table.js
@@ -1,66 +1,30 @@
 function onButtonClick() {
   var csvData = document.getElementById('csvtext').value;
-  var setData = document.getElementById('headertext').value;
-  if (setData.length !== 0) {
-    localStorage.setItem('setData', setData);
-  }
-  if (document.getElementById('result') !== null) {
-    var element = document.getElementById('result');
-    element.textContent = null;
-  }
   var lfArray = csvData.split("\n");
-  if (document.getElementById('checkbox').checked) {
-    var csvArray = [];
-    for (var i = 1; i < lfArray.length; i++) {
-      var row = lfArray[i];
-      var commaArray = row.split(",");
-      csvArray.push(commaArray);
-    }
-    var table = document.createElement('table');
+  var csvArray;
+  setData();
+  if (document.getElementById('result') !== null) {
+    clear();
+  }
+  function createCsvArray() {
+    var row = lfArray[i];
+    var commaArray = row.split(",");
+    csvArray.push(commaArray);
+  }
+  var table = document.createElement('table');
+  function createTable() {
     var result = document.getElementById('result');
     result.appendChild(table);
-
-    var headerArray = lfArray[0].split(",");
+  }
+  function createHeader() {
     for (var h = 0; h < headerArray.length; h++) {
       var thResult = headerArray[h];
       var th = document.createElement('th');
       table.appendChild(th);
       th.innerText = thResult;
     }
-
-    for (var j = 0; j < csvArray.length; j++) {
-      var trResult = csvArray[j];
-      var tr = document.createElement('tr');
-      table.appendChild(tr);
-      for (var k = 0; k < trResult.length; k++) {
-        var tdResult = trResult[k];
-        var td = document.createElement('td');
-        tr.appendChild(td);
-        td.innerText = tdResult;
-      }
-    }
-  } else {
-
-    var headerData = document.getElementById('headertext').value;
-    var csvArray = [];
-    for (var i = 0; i < lfArray.length; i++) {
-      var row = lfArray[i];
-      var commaArray = row.split(",");
-      csvArray.push(commaArray);
-    }
-
-    var table = document.createElement('table');
-    var result = document.getElementById('result');
-    result.appendChild(table);
-
-    var headerArray = headerData.split(",");
-    for (var h = 0; h < headerArray.length; h++) {
-      var thResult = headerArray[h];
-      var th = document.createElement('th');
-      table.appendChild(th);
-      th.innerText = thResult;
-    }
-
+  }
+  function createData() {
     for (var j = 0; j < csvArray.length; j++) {
       var trResult = csvArray[j];
       var tr = document.createElement('tr')
@@ -73,9 +37,41 @@ function onButtonClick() {
       }
     }
   }
+  if (document.getElementById('checkbox').checked) {
+    csvArray = [];
+    for (var i = 1; i < lfArray.length; i++) {
+      createCsvArray();
+    }
+    createTable();
+    var headerArray = lfArray[0].split(",");
+    createHeader();
+    createData();
+  } else {
+    var headerData = document.getElementById('headertext').value;
+    csvArray = [];
+    for (var i = 0; i < lfArray.length; i++) {
+      createCsvArray();
+    }
+    createTable();
+    var headerArray = headerData.split(",");
+    createHeader();
+    createData();
+  }
 }
 
 window.onload = function () {
   var getData = localStorage.getItem('setData');
   document.getElementById("headertext").value = getData;
+}
+
+function setData() {
+  var setData = document.getElementById('headertext').value;
+  if (setData.length !== 0) {
+    localStorage.setItem('setData', setData);
+  }
+}
+
+function clear() {
+  var element = document.getElementById('result');
+  element.textContent = null;
 }
