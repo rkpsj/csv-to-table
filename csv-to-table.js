ce3e519e613b7b6995ffde527beb9d21abb9cbef
rkpsj <rkpsj789@gmail.com>
2017-11-28 09:17:36 +0000

    localStorageにキーを3つ保存する

--- csv-to-table.js
+++ csv-to-table.js
@@ -57,18 +57,80 @@ function onButtonClick() {
     createHeader();
     createData();
   }
+  logData();
 }
 
+
 window.onload = function () {
-  var getData = localStorage.getItem('setData');
-  document.getElementById("headertext").value = getData;
+  var getData1 = localStorage.getItem('setData1');
+  var getData2 = localStorage.getItem('setData2');
+  var getData3 = localStorage.getItem('setData3');
+  if (getData1 === null) {
+    return;
+  }
+  document.getElementById("headertext").value = getData1;
+  logData();
 }
 
 function setData() {
-  var setData = document.getElementById('headertext').value;
-  if (setData.length !== 0) {
-    localStorage.setItem('setData', setData);
+  var headerText = document.getElementById('headertext').value;
+  var getData1 = localStorage.getItem('setData1');
+  var getData2 = localStorage.getItem('setData2');
+  var getData3 = localStorage.getItem('setData3');
+  if (headerText === null || headerText === "") {
+    return;
   }
+  if (getData1 === headerText) {
+    return;
+  }
+  if (getData1 === null) {
+    setData1 = headerText;
+    localStorage.setItem('setData1', setData1);
+
+  } else if (getData2 === null) {
+    setData2 = getData1;
+    localStorage.setItem('setData2', setData2);
+    setData1 = headerText;
+    localStorage.setItem('setData1', setData1);
+  } else if (getData2 === headerText) {
+    setData2 = getData1;
+    localStorage.setItem('setData2', setData2);
+    setData1 = headerText;
+    localStorage.setItem('setData1', setData1);
+  } else {
+    setData3 = getData2;
+    localStorage.setItem('setData3', setData3);
+    setData2 = getData1;
+    localStorage.setItem('setData2', setData2);
+    setData1 = headerText;
+    localStorage.setItem('setData1', setData1);
+  }
+}
+
+function logData() {
+  var getData1 = localStorage.getItem('setData1');
+  var getData2 = localStorage.getItem('setData2');
+  var getData3 = localStorage.getItem('setData3');
+  var log1 = document.getElementById('log1')
+  var log2 = document.getElementById('log2')
+  var log3 = document.getElementById('log3')
+  log1.innerText = getData1;
+  log2.innerText = getData2;
+  log3.innerText = getData3;
+}
+
+
+function log1() {
+  var getData1 = localStorage.getItem('setData1');
+  document.getElementById("headertext").value = getData1;
+}
+function log2() {
+  var getData2 = localStorage.getItem('setData2');
+  document.getElementById("headertext").value = getData2;
+}
+function log3() {
+  var getData3 = localStorage.getItem('setData3');
+  document.getElementById("headertext").value = getData3;
 }
 
 function resultClear() {
