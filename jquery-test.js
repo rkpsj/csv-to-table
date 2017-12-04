40ed01510c7c00e703005307bfa5df1547cfb0bb
eto <hiroaki.eto@tareha.com>
2017-12-04 19:35:00 +0900

    ヘッダーデータの表示（jQuery）

--- jquery-test.js
+++ jquery-test.js
@@ -1,27 +1,38 @@
 $(function () {
-  var $text = $("#csvtext");
-  $("#textClear").click(function () {
-    $text.val(null);
+  $('#textClear').click(function () {
+    $('#csvtext').val(null);
   });
-  var $result = $("#result");
-  $("#resultClear").click(function () {
-    $result.empty();
+  $('#resultClear').click(function () {
+    $('#result').empty();
   });
-  $("#resultButton").click(function () {
-    var $lfArray = $text.val().split('\n');
-    console.log($lfArray);
-    if ($text.val() === null || $text.val() === "") {
+  $('#resultButton').click(function () {
+    var lfArray = $('#csvtext').val().split('\n');
+    if ($('#csvtext').val() === null || $('#csvtext').val() === '') {
       return;
     }
-    $result.empty();
-    $("<table id='table' class='table table-striped table-bordered'>").appendTo($result);
-    $("<tbody id='tbody'>").appendTo("#table");
-    if ($("#checkbox").prop("checked")) {
-      var $hdArray = $lfArray[0].split(',');
-      console.log($hdArray);
+    $('#result').empty();
+    $('<table id="table" class="table table-striped table-bordered">').appendTo($('#result'));
+    $('<thead id="thead">').appendTo('#table');
+    $('<tbody id="tbody">').appendTo('#table');
+    if ($('#checkbox').prop('checked')) {
+      var hdArray = lfArray[0].split(',');
     } else {
-      var $hdArray = $("#headertext").val().split(',');
-      console.log($hdArray);
+      var hdArray = $('#headertext').val().split(',');
+    }
+    $('<tr id="tr">').appendTo('#thead');
+    for (var h = 0; h < hdArray.length; h++) {
+      var thResult = hdArray[h];
+      $('<th>').appendTo('#tr').text(thResult);
+    }
+    var csvArray = [];
+    if ($('#checkbox').prop('checked')) {
+      for (var i = 1; i < lfArray.length; i++) {
+        csvArray.push(lfArray[i].split(','));
+      }
+    } else {
+      for (var i = 0; i < lfArray.length; i++) {
+        csvArray.push(lfArray[i].split(','));
+      }
     }
   });
 });
