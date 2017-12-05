fe06e109d425b30aac57529876ce5c393acc72e9
eto <hiroaki.eto@tareha.com>
2017-12-05 13:21:42 +0900

    localStorageの実装（jQuery）
    ・手動入力のヘッダーデータをlocalStorageに保存
    ・読み込み時と表示ボタンクリック時に保存したデータを表示
    ・復元ボタンの実装

--- jquery-test.js
+++ jquery-test.js
@@ -42,5 +42,59 @@ $(function () {
         $('<td>' + tdResult + '</td>').appendTo(tr);
       }
     }
+    var getData = JSON.parse(localStorage.getItem('setData'));
+    if ($('#headertext').val() === null || $('#headertext').val() === '') {
+      return;
+    }
+    if (getData === null) {
+      var headerData = [$('#headertext').val()];
+      localStorage.setItem('setData', JSON.stringify(headerData));
+    } else if (getData[0] !== null) {
+      var getData = getData.filter(function (a) {
+        return a !== $('#headertext').val();
+      });
+      var headerData = getData.unshift($('#headertext').val());
+      localStorage.setItem('setData', JSON.stringify(getData));
+      if (getData.length > 3) {
+        var headerData = getData.pop();
+        localStorage.setItem('setData', JSON.stringify(getData));
+      }
+    }
+    restoreHeader();
   });
+  $('#restore1').click(function () {
+    var getData = JSON.parse(localStorage.getItem('setData'));
+    if (getData === null) {
+    }
+    $('#headertext').val(JSON.parse(localStorage.getItem('setData'))[0]);
+  });
+  $('#restore2').click(function () {
+    var getData = JSON.parse(localStorage.getItem('setData'));
+    if (getData === null) {
+    }
+    $('#headertext').val(JSON.parse(localStorage.getItem('setData'))[1]);
+  });
+  $('#restore3').click(function () {
+    var getData = JSON.parse(localStorage.getItem('setData'));
+    if (getData === null) {
+    }
+    $('#headertext').val(JSON.parse(localStorage.getItem('setData'))[2]);
+  });
+  restoreHeader();
 });
+
+function restoreHeader() {
+  var getData = JSON.parse(localStorage.getItem('setData'));
+  if (getData === null) {
+    return;
+  }
+  if (getData[0]) {
+    $('#restore1').html('<input type="button" class="btn-success" value="ヘッダー1を復元" /> ' + getData[0]);
+  }
+  if (getData[1]) {
+    $('#restore2').html('<input type="button" class="btn-success" value="ヘッダー2を復元" /> ' + getData[1]);
+  }
+  if (getData[2]) {
+    $('#restore3').html('<input type="button" class="btn-success" value="ヘッダー3を復元" /> ' + getData[2]);
+  }
+}
